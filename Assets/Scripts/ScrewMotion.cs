804c478cece2c784f87f0e52c208f69f51e14657
Ramakrishnan <ramkikumars@gmail.com>
2023-06-05 17:41:27 +0530

    Update

--- Assets/Scripts/ScrewMotion.cs
+++ Assets/Scripts/ScrewMotion.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Interhaptics.Utils;
+using Interhaptics.Internal;
 
 using UltimateXR.Manipulation;
 
@@ -12,15 +12,17 @@ public class ScrewMotion : MonoBehaviour
     [SerializeField] float pitch = 1;
     [SerializeField] private float duration;
     [SerializeField] private VelocityEstimator velocityEstimator;
-    [SerializeField] private AudioHapticSource rotatingHaptics;
-    [SerializeField] private AudioHapticSource squeakHaptics;
-    private Vector3 tempVect, newPos;
-    private float dist, unit_dist, intitialAngle, lastAngle, CurrentAngle, prevAngle, velocity, random;
-    public bool getSmoothedVelocity = true;
 
+    // [SerializeField] private EventHapticSource rotatingHaptics;
+    [SerializeField] private AudioSource rotatingAudio;
+    // [SerializeField] private AudioSource squeakAudio;
+    // [SerializeField] private EventHapticSource squeakHaptics;
+    [SerializeField] private float speed;
+    private Vector3 tempVect, newPos,prevAngle2;
+    private float dist, unit_dist, intitialAngle, lastAngle, CurrentAngle, prevAngle,velocity,random;
     float lerpTime = 1;
     private UxrGrabbableObject grabObj => GetComponentInChildren<UxrGrabbableObject>();
-
+    // private Quaternion prevAngle1;
     void Start()
     {
 
@@ -30,8 +32,10 @@ public class ScrewMotion : MonoBehaviour
         unit_dist = pitch / 360f;
 
         // rb = GetComponent<Rigidbody>();
-        grabObj.ConstraintsApplied += speedChange;
 
+        grabObj.ConstraintsApplied+=speedChange;
+        grabObj.Grabbed+=OnNutGrabbed;
+        grabObj.Released+=OnNutReleased;
 
     }
 
@@ -44,32 +48,29 @@ public class ScrewMotion : MonoBehaviour
         // }
 
 
-        
-        if (grabObj.IsBeingGrabbed)
-        {
-            Vector3 angularVelocity = UxrGrabManager.Instance.GetGrabbedObjectAngularVelocity(grabObj, getSmoothedVelocity);
-            // SET IS KINEMATIC TO FALSE
-            Debug.Log(angularVelocity.y);
-            // if rounded abosolute value of angular velocity is less than 0.5, stop the audio
+
+        // if (grabObj.IsBeingGrabbed)
+        // {
+        //     Vector3 angularVelocity = UxrGrabManager.Instance.GetGrabbedObjectAngularVelocity(grabObj, getSmoothedVelocity);
+        //     // SET IS KINEMATIC TO FALSE
+        //     Debug.Log(angularVelocity.y);
+        //     // if rounded abosolute value of angular velocity is less than 0.5, stop the audio
 
 
-            if (Mathf.RoundToInt(Mathf.Abs(angularVelocity.y)) < 20)
-            {
-                Debug.Log("STOP");
-                rotatingHaptics.Stop();
-            }
-            else if (!rotatingHaptics.audioSource.isPlaying)
-            {
-                rotatingHaptics.Play();
-            }
-        }
+        //     if (Mathf.RoundToInt(Mathf.Abs(angularVelocity.y)) < 20)
+        //     {
+        //         Debug.Log("STOP");
+        //         rotatingHaptics.Stop();
+        //     }
+        //     else if (!rotatingHaptics.audioSource.isPlaying)
+        //     {
+        //         rotatingHaptics.Play();
+        //     }
+        // }
         if (rotateObj.transform.hasChanged)
         {
 
-            // if (!rotatingHaptics.audioSource.isPlaying)
-            // {
-            //     rotatingHaptics.Play();
-            // }
+
             CurrentAngle = Mathf.Round(rotateObj.transform.localRotation.eulerAngles.y);
 
             if (Mathf.Abs(CurrentAngle - lastAngle) >= 2)
@@ -100,23 +101,45 @@ public class ScrewMotion : MonoBehaviour
             rotateObj.transform.hasChanged = false;
 
         }
-        else
-        {
-            rotatingHaptics.Stop();
+
+        else{
+            rotatingAudio.Stop();
+
+
         }
     }
     private IEnumerator WaitAndUnlock(float seconds)
     {
         yield return new WaitForSeconds(seconds);
         grabObj.IsLockedInPlace = true;
-        squeakHaptics.Play();
+        // squeakHaptics.Play();
         yield return new WaitForSeconds(seconds);
-        squeakHaptics.Stop();
+        // squeakHaptics.Stop();
         grabObj.IsLockedInPlace = false;
         // lastAngle = CurrentAngle;
     }
-    private void speedChange(object sender, UxrApplyConstraintsEventArgs e)
-    {
-        rotateObj.transform.eulerAngles.Set(0, 0, 0);
+
+    private void speedChange(object sender, UxrApplyConstraintsEventArgs e){
+        // float angle=rotateObj.transform.localEulerAngles.y;
+        Vector3 angle =prevAngle2+new Vector3(0,Time.deltaTime*speed,0);
+        // Debug.Log("SpeedChange");
+        rotateObj.transform.localRotation=Quaternion.Euler(angle);
+        prevAngle2=rotateObj.transform.eulerAngles;
+
     }
+
+
+public void OnNutGrabbed(object sender, UxrManipulationEventArgs e)
+    {
+        Debug.Log("Nut Grabbed");
+        rotatingAudio.Play();
+    }
+
+    public void OnNutReleased(object sender, UxrManipulationEventArgs e)
+    {
+        if(rotatingAudio.isPlaying){
+            rotatingAudio.Pause();
+        }
+    }
+
 }
