f10e53738fc30d18a1fb209fb9313fceea5f3bd6
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2023-06-24 12:10:40 +0530

    .
    ..

--- Assets/NetworkPlayerSpawner.cs
+++ Assets/NetworkPlayerSpawner.cs
@@ -9,7 +9,7 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        spawnedPlayerPrefab=PhotonNetwork.Instantiate("Cube",transform.position,transform.rotation);
+        spawnedPlayerPrefab=PhotonNetwork.Instantiate("Network Player",transform.position,transform.rotation);
     }
 
     public override void OnLeftRoom()
