d42ad7567dbc746fd9ae71f9e70561578394ca5d
Ramakrishnan <ramkikumars@gmail.com>
2023-07-06 02:21:15 +0530

    .

--- Assets/Scripts/Networking/NetworkGrabbable.cs
+++ Assets/Scripts/Networking/NetworkGrabbable.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 using System.Threading.Tasks;
+using SG;
 public class NetworkGrabbable : NetworkBehaviour
 {
     // Start is called before the first frame update
-    public bool ChangeState=false;
+    public SG_Grabable sgGrabable;
+    [Networked]
+    public NetworkString<_16> currentGrabbed { get; set; }
+    // [Networked]
+    // public NetworkString<_16> secondGrabbed { get; set; }
+    [Networked(OnChanged = nameof(OnChangedGrabber))]
+    public NetworkBool changeGrabber{ get; set; }
     void Start()
     {
-
+        sgGrabable.ObjectGrabbed.AddListener(ObjectGrabbed);
+        sgGrabable.ObjectReleased.AddListener(ObjectReleased);
     }
 
     // Update is called once per frame
@@ -27,5 +35,48 @@ public class NetworkGrabbable : NetworkBehaviour
         else Debug.Log("This Runner doesn't have StateAuthority");
     }
 
+    private void ObjectGrabbed(SG_Interactable obj1,SG_GrabScript obj2){
+
+        if(!Object.HasStateAuthority){
+            if(currentGrabbed!=""){
+                changeGrabber=!changeGrabber;
+            }
+            currentGrabbed=Object.Runner.LocalPlayer.ToString();
+            Debug.Log($"{this.gameObject.name} was grabbed by {currentGrabbed}");
+        }
+    }
+    private void ObjectReleased(SG_Interactable obj1, SG_GrabScript obj2)
+    {
+
+    }
+
+    async void ReqAuthorithy(NetworkObject o)
+    {
+        await WaitForStateAuthority(o);
+    }
+
+    public async Task<bool> WaitForStateAuthority(NetworkObject o, float maxWaitTime = 8)
+    {
+        float waitStartTime = Time.time;
+        o.RequestStateAuthority();
+        while (!o.HasStateAuthority && (Time.time - waitStartTime) < maxWaitTime)
+        {
+            await System.Threading.Tasks.Task.Delay(1);
+        }
+        return o.HasStateAuthority;
+    }
+
+    public static void OnChangedGrabber(Changed<NetworkGrabbable> changed)
+    {
+        changed.Behaviour.ChangedGrabber();
+    }
+
+    private void ChangedGrabber()
+    {
+        if(Object.HasStateAuthority){
+            sgGrabable.ReleaseSelf();
+            ReqAuthorithy(Object);
+        }
+    }
 
 }
